2f16a8d11b927f652cc372075b12f8155f19eaee
Vedant <70121054+Vedant2100@users.noreply.github.com>
2026-02-27 16:17:28 -0800

    Time: 327 ms (13.83%), Space: 46.2 MB (7.06%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # heap for keeping track of maximum
+        #O(N2) BRUTE
+        # since keeping track ofmaximum , heaps needed 
+        # O(NLOGN) with heap and thats the best. You could reconstruct heap everytme.
+        # Below greedy which only cares about removing from heap if its aminimum
+        # Its just heap istead of set and dict resp
         heap = []
         res = []
-
-        for i in range(len(nums)):
-            heapq.heappush(heap, (-nums[i], i))
-
-            while heap[0][1] <= i-k:
-                heapq.heappop(heap) # “If the maximum element’s index is outside the window, remove it.” lazy deletion
-            
-            if i >= k-1: # window completed, record max
+        for j in range(len(nums)):
+            heapq.heappush(heap, (-nums[j], j)) # single pass heap populating
+            if j >= k -1: #j outside 0 to k-1
+                while heap and heap[0][1] <= j-k: # remove minimums if outside window
+                    heapq.heappop(heap)
                 res.append(-heap[0][0])
         return res
-
         
