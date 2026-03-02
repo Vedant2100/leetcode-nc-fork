a5996643dba093a0ad5c48bdf2c65fc5385bfdeb
Vedant <70121054+Vedant2100@users.noreply.github.com>
2026-03-02 14:45:18 -0800

    Time: 7 ms (17.8%), Space: 19.3 MB (88.95%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        from collections import deque
+        # mutli source BFS
         q = deque()
-        # multi source BFS
-        fresh, time = 0, 0
         rows, cols = len(grid), len(grid[0])
-
-        # get sources and fresh count
+        fresh = 0
+        # collect sources and goals
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    fresh +=1
+                    fresh += 1
                 elif grid[r][c] == 2:
                     q.append((r,c))
+        time = 0
         
-        directions = [[0,1], [1,0], [-1,0], [0,-1]]
-        
-        while fresh > 0 and q: # while is one wave
-            for _ in range(len(q)): # missed 
-                r, c = q.popleft() # did pop
-                for dr, dc in directions:
-                    rn, cn = r+ dr, c+ dc
-                    if 0 <= rn < rows and 0 <= cn < cols and grid[rn][cn] == 1: # missed left side check 
-                        grid[rn][cn] = 2
-                        q.append((rn, cn))
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in [[1,0], [0,1], [-1,0], [0,-1]]:
+                    if 0<= r+dr < rows and 0<= c+dc < cols and grid[r+dr][c+dc] == 1:
+                        grid[r+dr][c+dc] = 2
                         fresh -= 1
-            time += 1 # after a wave which is the while loop
+                        q.append((r+dr, c+dc))
+            time += 1  
         return time if fresh == 0 else -1
-            
-            
+                
+
+        
