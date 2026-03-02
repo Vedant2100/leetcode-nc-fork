fed7f65c3bb9e0c59821724512e8a8d86718a23c
Vedant <70121054+Vedant2100@users.noreply.github.com>
2026-03-02 15:19:53 -0800

    Time: 11 ms (13.19%), Space: 21.6 MB (14.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,58 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #graph from course ot preq
-        adj = collections.defaultdict(list)
-        for c, p in prerequisites:
-            adj[c].append(p)
-        visited = set() #efficeincy
-        onpath = set()
-        def dfs(c):
-            if c in visited:
-                return True # no cycle, just skip
-            if c in onpath:
-                return False # cycle i
-            onpath.add(c)
-            for nei in adj[c]:
-                if not dfs(nei):
-                    return False
-            onpath.remove(c)
-            visited.add(c) # at the end 
-            return True
-        for c in range(numCourses):
-            if dfs(c) is False:
+        adjlist = collections.defaultdict(list)
+        for course, prereq in prerequisites:
+            adjlist[course].append(prereq)
+
+        courses = set(adjlist.keys())
+        visited = set()
+        onpath= set() # for cycle detection
+
+        def cycle(node):
+            if node in visited:
                 return False
-        return True
+            if node in onpath:
+                # print("hi")
+                return True
+            onpath.add(node)
+            for prereq in adjlist[node]:
+                # print(f"checking for {prereq} for {node}")
+                if cycle(prereq):
+                    return True
+            onpath.remove(node)
+            visited.add(node)
+            return False
+        cycleExists = False
+        for course in courses:
+            if cycle(course):
+                cycleExists = True
+        return not cycleExists
+
+
+
+
+        # #graph from course ot preq
+        # adj = collections.defaultdict(list)
+        # for c, p in prerequisites:
+        #     adj[c].append(p)
+        # visited = set() #efficeincy
+        # onpath = set()
+        # def dfs(c):
+        #     if c in visited:
+        #         return True # no cycle, just skip
+        #     if c in onpath:
+        #         return False # cycle i
+        #     onpath.add(c)
+        #     for nei in adj[c]:
+        #         if not dfs(nei):
+        #             return False
+        #     onpath.remove(c)
+        #     visited.add(c) # at the end 
+        #     return True
+        # for c in range(numCourses):
+        #     if dfs(c) is False:
+        #         return False
+        # return True
 
 
 
