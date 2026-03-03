aa4ef977f3d8b7499a29e2b148aaa3cc8f8fa28e
Vedant <70121054+Vedant2100@users.noreply.github.com>
2026-03-02 19:51:17 -0800

    Time: 1225 ms (5%), Space: 19.6 MB (86.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,18 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        countT = collections.Counter(t)
-        window = collections.Counter()
+        # s has to have greq length than t
+        if len(t) > len(s):
+            return ""
+        countT = Counter(t)
+        minLen = float("inf")
+        minSubst = ""
+        # we start froma. window of size t
+        countS = Counter() # empty 
         l = 0
-        res = ""
-        minLen = float('inf')
+        for j in range(len(s)): # we dont start from len(t) lol
+            countS[s[j]] += 1 
+            if countT <= countS:
+                while countT <= countS:
+                    if j-l+1 < minLen:
+                        minLen = j-l+1               
+                        minSubst = s[l:j+1]
+                    countS[s[l]] -= 1
+                    l += 1
+        return minSubst
 
-        for r in range(len(s)):
-            window[s[r]] += 1
-            while countT <= window:
-                if (r - l + 1) < minLen:
-                    minLen = r - l + 1
-                    res = s[l : r + 1]              
-                window[s[l]] -= 1
-                l += 1
-                
-        return res
+
